9edda0f042b0a867436b3647daeeb5ced38e3eb1
Werik Santos <werik.msti@gmail.com>
2024-11-03 18:46:35 -0300

    Mais implementações

---
+++ src/pages/Dashboard/Pedido.tsx
@@ -0,0 +1,11 @@
+import { Form } from "@/components";
+
+const Orcamento = () => {
+  return (
+    <div className="pedido-container">
+      <Form type={0} />
+    </div>
+  );
+};
+
+export default Orcamento;
