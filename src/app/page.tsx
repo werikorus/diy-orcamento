117cd935f594110d18cbef4ee5f595b052316ab4
Werik Santos <werik.msti@gmail.com>
2024-11-04 15:25:58 -0300

    .ENV file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,17 @@
 import Home from "@/pages/Home";
 import { MainDataContextProvider } from "@/Contexts";
+
 //import { initializeApp } from "firebase/app";
 //import { getAnalytics } from "firebase/analytics";
 
 //const firebaseConfig = {
-//  apiKey: "AIzaSyCyIxGygtTf-DvIR7MrSLQMt1G6-P1OyFY",
+//  apiKey: API_KEY_FIREBASE,
 //  authDomain: "diy-orcamento-db.firebaseapp.com",
 //  projectId: "diy-orcamento-db",
 //  storageBucket: "diy-orcamento-db.firebasestorage.app",
 //  messagingSenderId: "297744338646",
-//  appId: "1:297744338646:web:f6dd3c6a2f523b25408377",
-//  measurementId: "G-YJRZL8MYH7"
+//  appId: APP_ID,
+//  measurementId: MEASUREMENT_ID
 //};
 
 //const app = initializeApp(firebaseConfig);
