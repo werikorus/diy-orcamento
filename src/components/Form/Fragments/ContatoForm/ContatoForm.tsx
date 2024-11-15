8e0c463b6541acd2217f71f700fba201e0bb56b2
Werik Santos <werik.msti@gmail.com>
2024-11-15 12:04:14 -0300

    code refactory on contact form

--- src/components/Form/Fragments/ContatoForm/ContatoForm.tsx
+++ src/components/Form/Fragments/ContatoForm/ContatoForm.tsx
@@ -5,90 +5,92 @@ export const ContatoForm = () => {
   return (
     <div className={styles.formContainer}>
       <h2>Contato</h2>
-      <FormControl className={styles.formControl}>
-        <div className={styles.divGrade}>
+      <form noValidate autoComplete="off">
+        <FormControl className={styles.formControl}>
+          <div className={styles.divGrade}>
+            <TextField
+              className={styles.TextField}
+              label="CNPJ/CPF"
+              variant="outlined"
+              id="cnpj-cpf"
+            />
+            <TextField
+              className={styles.TextField}
+              label="Inscrição estadual"
+              variant="outlined"
+              id="state-subscription"
+            />
+          </div>
+
           <TextField
             className={styles.TextField}
-            label="CNPJ/CPF"
+            label="Razão Social"
             variant="outlined"
-            id="cnpj-cpf"
-          />
-          <TextField
-            className={styles.TextField}
-            label="Inscrição estadual"
-            variant="outlined"
-            id="state-subscription"
-          />
-        </div>
-
-        <TextField
-          className={styles.TextField}
-          label="Razão Social"
-          variant="outlined"
-          id="social-reazon"
-        />
-
-        <TextField
-          className={styles.TextField}
-          label="Nome Fantasia"
-          variant="outlined"
-          id="fantazy-name"
-        />
-
-        <div className={styles.divGrade}>
-          <TextField
-            className={styles.TextField}
-            label="Telefone"
-            variant="outlined"
-            id="phone"
+            id="social-reazon"
           />
 
           <TextField
             className={styles.TextField}
-            label="e-mail"
+            label="Nome Fantasia"
             variant="outlined"
-            id="email"
-          />
-        </div>
-
-        <div className={styles.divGrade}>
-          <TextField
-            className={styles.TextField}
-            label="CEP/Código postal"
-            variant="outlined"
-            id="codepost"
+            id="fantazy-name"
           />
 
-          <TextField
-            className={styles.TextField}
-            label="Endereço"
-            variant="outlined"
-            id="address"
-          />
-        </div>
+          <div className={styles.divGrade}>
+            <TextField
+              className={styles.TextField}
+              label="Telefone"
+              variant="outlined"
+              id="phone"
+            />
 
-        <TextField
-          className={styles.TextField}
-          label="Complemento"
-          variant="outlined"
-          id="complement"
-        />
+            <TextField
+              className={styles.TextField}
+              label="e-mail"
+              variant="outlined"
+              id="email"
+            />
+          </div>
 
-        <div className={styles.divGrade}>
+          <div className={styles.divGrade}>
+            <TextField
+              className={styles.TextField}
+              label="CEP/Código postal"
+              variant="outlined"
+              id="codepost"
+            />
+
+            <TextField
+              className={styles.TextField}
+              label="Endereço"
+              variant="outlined"
+              id="address"
+            />
+          </div>
+
           <TextField
             className={styles.TextField}
-            label="Cidade"
+            label="Complemento"
             variant="outlined"
-            id="city"
+            id="complement"
           />
-          <TextField
-            className={styles.TextField}
-            label="Estado"
-            variant="outlined"
-            id="estate"
-          />
-        </div>
-      </FormControl>
+
+          <div className={styles.divGrade}>
+            <TextField
+              className={styles.TextField}
+              label="Cidade"
+              variant="outlined"
+              id="city"
+            />
+            <TextField
+              className={styles.TextField}
+              label="Estado"
+              variant="outlined"
+              id="estate"
+            />
+          </div>
+        </FormControl>
+      </form>
     </div>
   );
 };
