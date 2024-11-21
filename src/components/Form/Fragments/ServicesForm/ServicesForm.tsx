836d7932a4152eb37db754b0ba3eb97b806ddf1c
Werik Santos <werik.msti@gmail.com>
2024-11-20 21:59:15 -0300

    Changes for services

--- src/components/Form/Fragments/ServicesForm/ServicesForm.tsx
+++ src/components/Form/Fragments/ServicesForm/ServicesForm.tsx
@@ -1,12 +1,14 @@
 import { TextField, FormControl, Button } from "@mui/material";
 import PlaylistAddIcon from "@mui/icons-material/PlaylistAdd";
 import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
-
 import styles from "./Services.module.css";
 import { useState } from "react";
+import { useMainDataContext, useDataForm } from "@/Hooks";
 
 export const ServicesForm = () => {
   const [items, setItems] = useState<JSX.Element[]>([]);
+  const { servicesValues } = useMainDataContext();
+  const { onSubmbitForm } = useDataForm();
 
   const createServiceItem = (
     addItem: (item: JSX.Element, index: number) => void,
@@ -60,38 +62,80 @@ export const ServicesForm = () => {
     setItems((prevItems) => prevItems.filter((_, i) => i !== index));
   };
 
+  const handleInitialItems = () => {
+    if (servicesValues.length > 0) {
+      return servicesValues.map((service, index) => (
+        <li
+          key={`service-item-${index}`}
+          className={`service-item ${styles.serviceItem}`}
+        >
+          <TextField
+            id="quantity-service"
+            className={styles.textQuantity}
+            variant="outlined"
+            type="number"
+            placeholder="0"
+            value={service.quantity}
+            onChange={() => onSubmbitForm(3)}
+          />
+          <TextField
+            id="description-service"
+            className={styles.textField}
+            label="Descrição"
+            variant="outlined"
+            type="text"
+            value={service.description}
+            onChange={() => onSubmbitForm(3)}
+          />
+          <TextField
+            id="price-service"
+            className={styles.textCurrency}
+            label="R$"
+            variant="outlined"
+            placeholder="R$"
+            type="number"
+            value={service.price}
+            onChange={() => onSubmbitForm(3)}
+          />
+        </li>
+      ));
+    }
+
+    return (
+      <li id="service-item" className={`service-item ${styles.serviceItem}`}>
+        <TextField
+          id="quantity-service"
+          className={styles.textQuantity}
+          variant="outlined"
+          type="number"
+          placeholder="0"
+        />
+        <TextField
+          id="description-service"
+          className={styles.textField}
+          label="Descrição"
+          variant="outlined"
+          type="text"
+        />
+        <TextField
+          id="price-service"
+          className={styles.textCurrency}
+          label="R$"
+          variant="outlined"
+          placeholder="R$"
+          type="number"
+        />
+      </li>
+    );
+  };
+
   return (
     <div className={styles.formContainer}>
       <h2>Serviços</h2>
       <form noValidate autoComplete="off">
         <FormControl className={styles.formControl}>
           <ul className={styles.lisControl}>
-            <li
-              id="service-item"
-              className={`service-item ${styles.serviceItem}`}
-            >
-              <TextField
-                id="quantity-service"
-                className={styles.textQuantity}
-                variant="outlined"
-                type="number"
-                placeholder="0"
-              />
-              <TextField
-                id="description-service"
-                className={styles.textField}
-                label="Descrição serviço"
-                variant="outlined"
-              />
-              <TextField
-                id="price-service"
-                className={styles.textCurrency}
-                placeholder="0"
-                label="R$"
-                variant="outlined"
-                type="number"
-              />
-            </li>
+            {handleInitialItems()}
             {items}
           </ul>
         </FormControl>
